79b7b45b9ef8870387d250f0ac52b2e66751a262
khireddine benchikh <65354259+khiro10@users.noreply.github.com>
2025-03-07 18:20:23 +0100

    Add files via upload

--- src/servers/countries.ts
+++ src/servers/countries.ts
@@ -6,6 +6,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const response = await axios.get('https://api.travelpayouts.com/data/en/countries.json');
     res.status(200).json(response.data);
+    console.log(req);
+    
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
