908dbfa987fd831505c46d29a1a3334748eb4df5
khireddine benchikh <65354259+khiro10@users.noreply.github.com>
2025-04-17 16:00:53 +0100

    Update place.tsx

--- src/pages/place.tsx
+++ src/pages/place.tsx
@@ -1,6 +1,6 @@
 import { Button, Card, Col, Empty, Layout, Rate, Row, Typography } from 'antd';
 import { Content } from 'antd/es/layout/layout';
-import React, {  useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import { useFetch } from '../components/fecthData';
 import { useNavigate, useParams } from 'react-router-dom';
 import { CloseOutlined } from "@ant-design/icons";
@@ -13,11 +13,48 @@ interface GeonameResponse {
   country: string;
 }
 const Place: React.FC = () => {
+          
 const [geo, setGeo] = useState<GeonameResponse>()    
 const navigate = useNavigate();
-const { id } = useParams<{ id: string }>();    
-const {data : geoData}  = useFetch(`https://travelyalla-backend-production.up.railway.app/api/country/${id}`);
-setGeo(geoData as any)
+const { id } = useParams<{ id: string }>();         
+// const {data : geoData}  = useFetch(`http://localhost:5000/api/country/${id}`);
+useEffect(() => {
+  const fetchCountry = async () => {
+          try {
+          const response = await axios.get(`https://travelyalla-backend-production.up.railway.app/api/country/${id} `, {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+  },
+});
+const data = await response.data;
+setGeo(data)            
+  } catch (error) {
+            console.error("Error fetching countries:", error);setLoading(false);
+          }
+        };
+    
+        fetchCountries();     
+  if (geoData) {
+    setGeo(geoData as GeonameResponse);
+  }
+}, [geoData]);
+
+useEffect(() => {
+
+  if (!document.getElementById("flight-script")) {
+    const flightScript = document.createElement("script");
+    flightScript.src=`https://tp.media/content?currency=usd&trs=390781&shmarker=607907&show_hotels=true&default_destination=${id}&powered_by=false&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%2332a8dd&color_button=%23FF2626ff&color_icons=%2332a8dd&dark=%23262626&light=%23FFFFFF&secondary=%23FFFFFF&special=%23C4C4C4&color_focused=%2332a8dd&border_radius=0&no_labels=true&plain=false&promo_id=7879&campaign_id=100`
+
+    flightScript.async = true;
+    flightScript.charset = "utf-8";
+    flightScript.id = "flight-script";
+    document.getElementById("flight-widget")?.appendChild(flightScript);
+  }
+  console.log(id);
+  
+}, []);
+
 // const t1 = "interesting_places";
 // const t2 = "architecture";
 // const t3 = "museums";
@@ -27,9 +64,9 @@ setGeo(geoData as any)
 // const [architecture, setArchitecture] = useState([])
 // const [museums, setMuseums] = useState([])
 // const [historical_places, sethistorical_places] = useState([])
-console.log(geoData);
+console.log(geo);
 
-const {data}  = useFetch(`https://travelyalla-backend-production.up.railway.app/api/countryplaces/${geo?.lat}/${geo?.lon}`);
+const {data}  = useFetch(`http://localhost:5000/api/countryplaces/${geo?.lat}/${geo?.lon}`);
 // useEffect(() => {
 //   if(data){
 //     setInteresting_places((data as any).features?.filter((place: any) => place.properties?.kinds?.includes(t1)))
@@ -53,14 +90,18 @@ return (
       <br />
       {/* <div min-width={300} style={{ width:"100%",height:400, objectFit: "cover", borderRadius: 10 }}><PixabayImage query={id}/></div> */}
       <p style={{textAlign :"left"}}></p>
+      <Typography.Title level={3}>Flights</Typography.Title>
+            <Row gutter={[16, 16]} id="flight-widget" style={{width:"100%"}}>
+       
+              </Row>
       <Typography.Title level={3}>interesting places</Typography.Title>
       <Row gutter={[16, 16]} className="list1">
             
-     {data && (data as any).features.slice(0,22)
+     {data && (data as any).features.slice(0,10)
       .map((place: any, index: number) => (
               <div key={index}>
               {place.properties.name && 
-              <Col xs={24} sm={12} md={8} onClick={()=> navigate(`/city/${place?.properties?.name}`)} >
+              <Col xs={24} sm={12} md={8}  onClick={()=> navigate(`/city/${place?.properties?.name}`)} >
                 <Card 
                     hoverable
                     style={{ width: 240,height: 360 , cursor: 'pointer',alignItems: 'center', justifyContent: 'center' }}
